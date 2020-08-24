2462580556d4d582c2d0c2379434e1a7f591028a
Sokolov Aleksey <amsokolov79@gmail.com>
2020-08-24 19:06:46 +0700

    Глава 5

--- Chapter5/Example1/javascripts/app.js
+++ Chapter5/Example1/javascripts/app.js
@@ -1,32 +1,30 @@
 var main = function () {
     "use strict";
-
+    console.log("Hello, World!");
     $.getJSON("cards/aceOfSpades.json", function (card) {
-        // create an element to hold the card
+ // создаем элемент для хранения карты
         var $cardParagraph = $("<p>");
-
-        // create the 
+ // создаем текст для карты
         $cardParagraph.text(card.rank + " of " + card.suit);
-
-        // append the card paragraph to main
+ // присоединяем абзац с картой к элементу main
         $("main").append($cardParagraph);
     });
-
     $.getJSON("cards/hand.json", function (hand) {
         var $list = $("<ul>");
-
-        // hand is an array, so we can iterate over it
+ // hand — массив, поэтому мы можем применить к нему итерационный процесс
+ // с помощью цикла forEach
         hand.forEach(function (card) {
-            // create a list item to hold the card
-            // and append it to the list
-            var $card = $("<li>");
-            $card.text(card.rank + " of " + card.suit);
-            $list.append($card);
-        });
-
-        // append the list to main
+ // создаем элемент списка для хранения карты
+ // и присоединяем его к списку
+        var $card = $("<li>");
+         $card.text(card.rank + " of " + card.suit);
+         $list.append($card);
+     });
+ // присоединяем список к элементу main
         $("main").append($list);
     });
 };
-
 $(document).ready(main);
+
+
+     
