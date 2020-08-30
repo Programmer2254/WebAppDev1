8d74ada15d1462a8faffad0996bc3cdfbc109ea5
Programmer2254 <amsokolov79@gmail.com>
2020-08-30 11:19:04 +0700

    255

--- Chapter5/Amazeriffic/javascripts/app.js
+++ Chapter5/Amazeriffic/javascripts/app.js
@@ -1,3 +1,13 @@
+var main = function (toDoObjects) {
+ "use strict";
+ // как main имеет доступ к списку задач!
+};
+$(document).ready(function () {
+ $.getJSON("todos.json", function (toDoObjects) {
+ // вызов функции main с аргументом в виде объекта toDoObjects
+ main(toDoObjects);
+ });
+});
 var main = function () {
     var toDos = ["Get groceries",
                  "Make up some new ToDos",
